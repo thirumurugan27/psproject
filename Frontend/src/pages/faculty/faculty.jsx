d98f0b01cfdb44b27cda9264598a9349767cb698
Gowthamj7773 <gowthamj7773@gmail.com>
2025-04-21 16:25:22 +0530

    frontend code only , added

--- Frontend/src/pages/faculty/faculty.jsx
+++ Frontend/src/pages/faculty/faculty.jsx
@@ -37,35 +37,39 @@ function Faculty() {
     return (
         <div className={styles.mainBox}>
             <div className={styles.hero}>
-                <img style={{width:45,height:45,display:"flex"}} src={ps}/>
-                <h4 style={{display:"flex",marginLeft:15}}>PS Mentorship</h4>
+                <img style={{width:45,height:45,display:"flex",margin:10}} src={ps}/>
+                <h4 style={{display:"flex"}}>PS Mentorship</h4>
             </div>
 
             {/* below is for map() */}
             <div>
                 <div className={styles.cardsContainer}>
                     <div className={styles.card}>
-                        <div style={{flexDirection:"row",display:"flex"}}>
-                        <div style={{display:"flex" , flexDirection:"row",flex:1}}>
+                        <div style={{flexDirection:"row",display:"flex",flex:5}}>
+                        <div style={{display:"flex" , flexDirection:"row",flex:1,margin:5}}>
                             <div style={{display:"flex",flex:1}}>
-                                <img src={userPS} style={{width:90,height:90, margin:10}}/>
+                                <img src={userPS} style={{width:95,height:95, margin:10}}/>
                             </div>
                             <div className={styles.details}>
-                                <div style={{marginTop:15}}>
+                                <div style={{marginTop:15,width:"auto"}}>
                                     <h3>userName</h3>
-                                    <p>Mentorship applied for</p>
+                                    <p style={{fontSize:13}}>Mentorship applied for</p>
                                     <h4>name of the course</h4>
                                 </div>
                             </div>
                         </div>
-                        <div style={{display:"flex",flexDirection:"row",alignItems:"center"}}>
+                        </div>
+                        <div style={{display:"flex",flexDirection:"row",alignItems:"center",flex:3}}>
                             <div  className={styles.button1} ><p>Approve</p></div>
                             <div className={styles.button2} ><p>Reject</p></div>
                         </div>
-                        </div>
                     </div>
+                    
+                    
                 </div>
             </div>
+
+            
         </div>
     )
 }
