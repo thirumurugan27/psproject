5185c2d98d3fcf24672de07718d620a35a27384b
thirumurugan27 <thirumurugan0200702gmail.com>
2025-05-11 08:52:06 +0530

    after autoupdate.js added

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -5,6 +5,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const PORT = process.env.PORT;
+const { runExpireFunctions } = require("./controllers/autoupdatestatus");
 
 app.use(express.json());
 app.use(cors());
@@ -15,6 +16,7 @@ const facultyRoutes = require("./controllers/faculty");
 const mentorRoutes = require("./controllers/mentor");
 const menteeRoutes = require("./controllers/mentee");
 
+
 app.use("/auth", authRoutes);
 app.use("/faculty", facultyRoutes);
 app.use("/mentor", mentorRoutes);
@@ -22,7 +24,7 @@ app.use("/mentee", menteeRoutes);
 app.use("/student", studentRoutes);
 
 
-
+runExpireFunctions();
 app.listen(PORT, () =>
   console.log(`🚀server running successfully on http://localhost:${PORT}`)
 );
